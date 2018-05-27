a757a3b9e3396cdfb01ec45ae34a17e5507f948b
Gande.Rohith <gande.rohith@cognizant.com>
2018-05-27 13:33:43 +0000

    uptill serviccontrollere

---
+++ movieCruiserByRohith/Controllers/MovieController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using movieCruiserByRohith.Services;
+
+namespace movieCruiserByRohith.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Movie")]
+    public class MovieController : Controller
+    {
+        private readonly IMovieService _service;
+
+        public MovieController(IMovieService service)
+        {
+            _service = service;
+        }
+
+        // GET: api/Movie
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var result = _service.GetAllMovies();
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return NotFound("There is an Error");
+            }
+        }
+
+        //// GET: api/Movie/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST: api/Movie
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT: api/Movie/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ test/MovieControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using movieCruiserByRohith.Services;
+using movieCruiserByRohith.Data.Models;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using movieCruiserByRohith.Controllers;
+
+namespace test
+{
+    public class MovieControllerTest
+    {
+        [Fact]
+        public void GetMethodWithoutParameter_ShouldReturnListOfMovie()
+        {
+            //Arrange
+            var mockService = new Mock<IMovieService>();
+            mockService.Setup(service => service.GetAllMovies()).Returns(this.GetMovies());
+            var controller = new MovieController(mockService.Object);
+
+            //Act
+            var result = controller.Get();
+
+            //Assert
+            var actionresult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<Movie>>(actionresult.Value);
+
+        }
+
+        private List<Movie> GetMovies()
+        {
+            var movie = new List<Movie>();
+
+            movie.Add(new Movie { Id = 1, Name = "movie1", Comments = "comments1", PosterPath = "path1", ReleaseDate = "releaseDate1", VoteAverage = 5.00, VoteCount = 100 });
+            movie.Add(new Movie { Id = 2, Name = "movie2", Comments = "comments2", PosterPath = "path2", ReleaseDate = "releaseDate2", VoteAverage = 6.00, VoteCount = 200 });
+            movie.Add(new Movie { Id = 3, Name = "movie3", Comments = "comments3", PosterPath = "path3", ReleaseDate = "releaseDate3", VoteAverage = 7.00, VoteCount = 300 });
+
+            return movie;
+        }
+    }
+}
